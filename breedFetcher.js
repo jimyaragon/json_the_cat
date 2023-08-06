e0c4bbbbe65bf9148cb6dab3f86b6cb275137a91
jimyaragon <jimy.aragon@gmail.com>
2023-08-05 19:46:41 -0600

    adding the mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,22 @@
 const request = require('request');
 
-const fetchBreedData = (breedName) => {
+const fetchBreedDescription = function(breedName, callback) {
   const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(apiUrl, (error, response, body) => {
     if (error) {
-      console.error('Error fetching data:', error);
+      callback(error, null);
     } else {
       const data = JSON.parse(body);
-
-      // Check if data is an array and not empty
       if (Array.isArray(data) && data.length > 0) {
         const firstEntry = data[0];
         const description = firstEntry.description;
-        console.log('Description:', description);
+        callback(null, description);
       } else {
-        console.log('No data found for the breed:', breedName);
+        callback(`No data found for the breed: ${breedName}`, null);
       }
     }
   });
 };
 
-// Read the breed name from the command-line arguments
-const args = process.argv.slice(2);
-const breedName = args[0];
-
-// Call the fetchBreedData function with the specified breed name
-fetchBreedData(breedName);
+module.exports = { fetchBreedDescription };
